c2a926006604b7b169536dfb394d4a24bcb6388f
sherissago <31224464+sherissago@users.noreply.github.com>
2017-10-13 14:29:48 -0700

    updated class

--- Pig.java
+++ Pig.java
@@ -1,4 +1,25 @@
-class Pig //implements Animal 
+class Pig implements Animal 
 {     
-     //your code here
+     private String myType;
+     private String mySound;
+     public Pig(String type, String sound)
+     {
+          myType = type;
+          mySound = sound;
+     }
+     
+     public Pic()
+     {
+          myType = "unknown";
+          mySound = "unknown";
+     }
+     
+     public String getSound()
+     {
+          return mySound;
+     }
+     public String getType()
+     {
+          return myType;
+     }
 }
