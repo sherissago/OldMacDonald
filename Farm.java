e4763d609a0d32d47898b6238b32636dd7a5dc77
sherissago <31224464+sherissago@users.noreply.github.com>
2017-10-16 16:30:37 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,4 +1,16 @@
 class Farm 
 {     
-   //your code here
+   private Animal[] aunchofAnimals = new Animal[3];
+   public Farm()
+   {
+      aBunchOfAnimals[0] = new Cow("cow","moo");
+      aBunchOfAnimals[1] = new Chick("chick","peep");
+      aBunchOfAnimals[2] = new Pig("pig","oink");
+   }
+   
+   pulic void animalSounds()
+      for(int nI = 0; nI < aBunchOfAnimals.length; nI++)
+      {
+         System.out.println(aBunchOfAnimals.getType() +" goes " +aBunchOfAnimals.getSound)
+      }
 }
