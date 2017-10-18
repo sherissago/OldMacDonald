1169cf27f97c1190e3b5f92dbeb071817df44103
sherissago <31224464+sherissago@users.noreply.github.com>
2017-10-18 14:34:56 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,26 +1,23 @@
 class Chick implements Animal 
-{     
-     private String myType;
-     private String mySound;
-     public Chick(String type, String sound)
-     {
-          myType = type;
-          mySound = sound;
-     }
-     public Chick()
-     {
-       myType = "unknown";
-       mySound = "unknown";
-     }
-     public String getSound()
-     {
-       return mySound;
-     }
-     
-     public String getType()
-     {
-       return myType;
-     }
-     
-     
-}
+{
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound)
+  {
+    myType = type;
+    mySound = sound;
+  }
+  public Chick()
+  {
+    myType = "unknown";
+    mySound = "unknown";
+  }
+  public String getSound()
+  {
+    return mySound;
+  }
+  public String getType()
+  {
+    return myType;
+  }
+  }
